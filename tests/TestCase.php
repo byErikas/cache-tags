525002bfa0341d70b01ac0fcfd0c1f84ba242fec
Erikas Byštautas <55389436+byErikas@users.noreply.github.com>
2025-03-08 13:16:21 +0200

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,14 +4,28 @@ namespace Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Illuminate\Contracts\Config\Repository as ConfigRepository; 
 use Illuminate\Support\Str;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected Repository $cache;
+    protected CacheRepository $cache;
 
-    protected function cache(): Repository
+    protected function defineEnvironment($app) 
+    {
+        // Setup cache store to use redis-tags driver
+        tap($app["config"], function (ConfigRepository $config) { 
+            $config->set("cache.default", "redis"); 
+            $config->set("cache.stores.redis", [ 
+                "driver"   => "redis-tags", 
+                "connection" => "default",
+                "lock_connection" => "default",
+            ]); 
+        });
+    }
+    
+    protected function cache(): CacheRepository
     {
         return Cache::store("redis");
     }
