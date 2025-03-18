be36ae522a7cc1bf43375edee6d8f56c4f58c605
eribys <erikas.bystautas@tutotoons.com>
2025-03-18 08:53:10 +0000

    remove workflow, set test case to default

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,16 @@
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Contracts\Cache\Repository as CacheRepository;
-use Illuminate\Contracts\Config\Repository as ConfigRepository; 
+use \Illuminate\Contracts\Cache\Repository;
 use Illuminate\Support\Str;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected CacheRepository $cache;
+    protected Repository $cache;
 
-    protected function defineEnvironment($app) 
-    {
-        // Setup cache store to use redis-tags driver
-        tap($app["config"], function (ConfigRepository $config) { 
-            $config->set("cache.default", "redis"); 
-            $config->set("cache.stores.redis", [ 
-                "driver"   => "redis-tags", 
-                "connection" => "default",
-                "lock_connection" => "default",
-            ]); 
-        });
-    }
-
-    protected function getPackageProviders($app)
-    {
-		return ["ByErikas\CacheTags\ServiceProvider"];
-	}
-    
-    protected function cache(): CacheRepository
+    protected function cache(): Repository
     {
         return Cache::store("redis");
     }
