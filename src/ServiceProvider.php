bb583ab98d236468e5baeda15861ae192ff53195
Jakub Potocký <potockyjakub@gmail.com>
2025-03-30 15:00:42 +0200

    Optimize extending cache with redis-tags store

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace ByErikas\CacheTags;
 
 use ByErikas\CacheTags\Cache\Store;
+use Illuminate\Contracts\Cache\Factory;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Facades\Cache;
@@ -14,8 +15,8 @@ class ServiceProvider extends IlluminateServiceProvider
      */
     public function register()
     {
-        $this->app->afterResolving("cache", function () {
-            Cache::extend("redis-tags", function (Application $app) {
+        $this->app->afterResolving("cache", static function (Factory $factory) {
+            $factory->extend("redis-tags", static function (Application $app) {
                 return Cache::repository(new Store($app["redis"], $app["config"]["cache.prefix"], $app["config"]["cache.stores.redis"]["connection"]));
             });
         });
