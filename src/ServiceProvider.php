f568f9e968ec06cacfe3720c30d95bfbcc476e0c
Jakub Potocký <potockyjakub@gmail.com>
2025-03-30 15:04:07 +0200

    CS fix

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,7 +6,6 @@ use ByErikas\CacheTags\Cache\Store;
 use Illuminate\Contracts\Cache\Factory;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Support\Facades\Cache;
 
 class ServiceProvider extends IlluminateServiceProvider
 {
@@ -16,8 +15,10 @@ class ServiceProvider extends IlluminateServiceProvider
     public function register()
     {
         $this->app->afterResolving("cache", static function (Factory $factory) {
-            $factory->extend("redis-tags", static function (Application $app) {
-                return Cache::repository(new Store($app["redis"], $app["config"]["cache.prefix"], $app["config"]["cache.stores.redis"]["connection"]));
+            $factory->extend("redis-tags", function (Application $app) {
+                /** @var Factory $this */
+
+                return $this->repository(new Store($app["redis"], $app["config"]["cache.prefix"], $app["config"]["cache.stores.redis"]["connection"]));
             });
         });
     }
